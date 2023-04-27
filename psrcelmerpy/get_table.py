01b9e63cd01a5bee70b250a32d94fb8e743dca56
Chris Peak <cpeak@psrc.org>
2023-04-27 16:07:03 -0700

    add in basic testing

--- psrcelmerpy/get_table.py
+++ psrcelmerpy/get_table.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 def get_table(schema, table_name, db_name='Elmer'):
     """
-    Return a table from a database.
+    Return a table or view from a database.
     
     Parameters
     ----------
     schema : str
-        The schema that the table lives in.
+        The schema that the table or view lives in.
 
     table_name : str
-        The name of the table you wish to retrieve
+        The name of the table or view that you wish to retrieve
         
     db_name : str
         The name of the database you are pulling from.  Defaults to 'Elmer'
@@ -19,7 +19,7 @@ def get_table(schema, table_name, db_name='Elmer'):
     Returns
     -------
     df
-        A data frame representation of the table db_name.schema.table_name 
+        A data frame representation of the table/view db_name.schema.table_name 
     """
     try:
        conn = get_conn(db_name)
